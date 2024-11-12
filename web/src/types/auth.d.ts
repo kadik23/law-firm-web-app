0584a2c33d9ac46fc235601b2f138f95014a5c42
unknown <kadiksalah03@gmail.com>
2024-11-12 17:01:47 +0100

    add validation for signup form

---
+++ web/src/types/auth.d.ts
@@ -0,0 +1,19 @@
+type SignupformType ={
+    stepOne:{
+        name: string;
+        surname: string;
+        email: string;
+        password: string;
+        conpassword: string;
+    }
+    stepTwo:{
+        nbr_tel:string;
+        pays: string;
+        ville: string;
+        age:string;
+        gender: string
+    }
+    stepThree?: {
+        files: unknown[]
+    }
+}
