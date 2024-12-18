a8ea8b4fea44ac449863506466db57aed3c16053
Salah Eddine Kadik <123268700+kadik23@users.noreply.github.com>
2024-12-18 15:50:04 +0100

    Update Users.js

--- LawyerBackend/models/Users.js
+++ LawyerBackend/models/Users.js
@@ -40,8 +40,8 @@ module.exports = (sequelize, DataTypes) => {
     allowNull: true
   },
   sex: {
-    type: DataTypes.STRING(10), 
-    allowNull: true
+      type: DataTypes.ENUM('Homme', 'Femme'),
+      allowNull: true
   },
   
   terms_accepted: {
@@ -56,4 +56,69 @@ module.exports = (sequelize, DataTypes) => {
 
     return User;
   };
-  
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+           - id
+ *         - name
+ *         - surname
+ *         - email
+ *         - password
+ *         - phone_number
+ *         - pays
+ *         - ville
+ *         - age
+ *         - sex
+ *         - terms_accepted
+ *         - type
+ *       properties:
+           id:
+             type: number
+             example: "2"
+ *         name:
+ *           type: string
+ *           maxLength: 20
+ *           example: "John"
+ *         surname:
+ *           type: string
+ *           maxLength: 20
+ *           example: "Doe"
+ *         email:
+ *           type: string
+ *           format: email
+ *           maxLength: 50
+ *           example: "john.doe@example.com"
+ *         password:
+ *           type: string
+ *           example: "password123"
+ *         phone_number:
+ *           type: string
+ *           maxLength: 20
+ *           example: "+1234567890"
+ *         pays:
+ *           type: string
+ *           example: "France"
+ *         ville:
+ *           type: string
+ *           example: "Paris"
+ *         age:
+ *           type: integer
+ *           example: 30
+ *         sex:
+ *           type: string
+ *           enum:
+ *             - Homme
+ *             - Femme
+ *           example: "Homme"
+ *         terms_accepted:
+ *           type: boolean
+ *           example: true
+ *         type:
+ *           type: string
+ *           example: "admin"
+ */
