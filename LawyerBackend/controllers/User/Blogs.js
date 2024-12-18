70dbec3f870ab4bb91e89fe81e5a205ebf3b65bb
Salah Eddine Kadik <123268700+kadik23@users.noreply.github.com>
2024-12-18 15:35:40 +0100

    Discard changes in Blogs.js

--- LawyerBackend/controllers/User/Blogs.js
+++ LawyerBackend/controllers/User/Blogs.js
@@ -31,6 +31,53 @@ const getAllBlogs= async (req,res)=>{
         res.status(500).send('Internal Server Error');
     }
 };
+/**
+ * @swagger
+ * /user/blogs/:id:
+ *   get:
+ *     summary: Retrieve a single blog by ID
+ *     description: This endpoint retrieves a single blog entry from the database based on the provided blog ID in the body.
+ *     tags:
+ *       - Blogs
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 example: 1
+ *                 description: The ID of the blog to retrieve
+ *     responses:
+ *       200:
+ *         description: A single blog entry
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Blog'
+ *       404:
+ *         description: Blog not found - The blog with the given ID does not exist
+ *       500:
+ *         description: Internal Server Error - An error occurred while fetching the blog
+ */
+
+const getBlogById= async (req,res)=>{
+    try {
+        const {id} = req.params;
+        let blog = await blogs.findOne({ where: { id } });
+
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog not found' });
+        }
+        return res.status(200).send(blog);
+    } catch (e) {
+        console.error('Error fetching blog', e);
+        res.status(500).send('Internal Server Error');
+    }
+};
 module.exports = {
-   getAllBlogs
+    getAllBlogs,
+    getBlogById
 };
