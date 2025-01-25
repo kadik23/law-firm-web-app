f1abac416f1359d4f112068806cda2bfb29cc6ba
kadik23 <kadiksalah03@gmail.com>
2025-01-25 14:01:23 +0100

    fix types errors

--- web/src/hooks/useAuth.ts
+++ web/src/hooks/useAuth.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "@/lib/utils/axiosClient";
+import { isAxiosError } from "axios";
 
 export const useAuth = () => {
   const [user, setUser] = useState<User | null>(null);
@@ -10,8 +11,8 @@ export const useAuth = () => {
       try {
         const response = await axios.get("/user/current", { withCredentials: true });
         setUser(response.data);
-      } catch (err: any) {
-        if (err.response?.status === 401) {
+      } catch (err: unknown) {
+        if (isAxiosError(err) && err.response?.status === 401) {
           console.warn("User not authenticated");
         } else {
           console.error("An unexpected error occurred:", err);
